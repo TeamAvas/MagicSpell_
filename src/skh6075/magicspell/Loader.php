add1da12f226efdac9b3806ba152ab22d7f4e5a8
Song ki ho <44698603+GodVas@users.noreply.github.com>
2022-02-09 00:53:33 +0900

    upload init

--- src/skh6075/magicspell/Loader.php
+++ src/skh6075/magicspell/Loader.php
@@ -1,1 +1,27 @@
 <?php
+
+declare(strict_types=1);
+
+namespace skh6075\magicspell;
+
+use InvalidArgumentException;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\SingletonTrait;
+
+final class Loader extends PluginBase{
+	use SingletonTrait;
+
+	public static function getInstance() : Loader{
+		if(self::$instance === null){
+			throw new InvalidArgumentException('Unable to check MagicSpell loading status.');
+		}
+		return self::$instance;
+	}
+
+	protected function onLoad() : void{
+		self::$instance = $this;
+	}
+
+	protected function onEnable() : void{
+	}
+}
